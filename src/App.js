ff4a953e45ab490633294258402728504fda6103
AmanAmbasta <kaman0353@gmail.com>
2021-11-19 14:14:21 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import TopNav from './components/topnav/TopNav';
+import Routes from './components/Routes';
+import { BrowserRouter, Route } from 'react-router-dom';
+// eslint-disable-next-line
+import { useSelector, useDispatch } from 'react-redux';
+import ThemeAction from './redux/actions/ThemeAction';
+const App = () => {
+  // const themeReducer = useSelector((state) => state.ThemeReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const themeClass = localStorage.getItem('themeMode', 'theme-mode-light');
+    const colorClass = localStorage.getItem('colorMode', 'theme-mode-light');
+    dispatch(ThemeAction.setMode(themeClass));
+    dispatch(ThemeAction.setColor(colorClass));
+  }, [dispatch]);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route
+        render={(props) => (
+          // <div className={`app ${themeReducer.mode} ${themeReducer.color}`}>
+          <div className={`app`}>
+            <Sidebar {...props} />
+            <div className="app__content">
+              <TopNav />
+              <div className="app__content-main">
+                <Routes />
+              </div>
+            </div>
+          </div>
+        )}
+      />
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+/* CSS */
+import './assets/css/grid.css';
+import './assets/css/theme.css';
+import './assets/css/index.css';
+import 'boxicons';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/reducers/ThemeReducer';
+const store = createStore(rootReducer);
+
+
+document.title = '|Admin Portal|The Classic Ladies|';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
